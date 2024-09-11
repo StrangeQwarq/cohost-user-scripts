cf53dc494bb6093a5e73dd2f5f91a7761207be2d
StrangeQwarq <118792687+StrangeQwarq@users.noreply.github.com>
2024-09-11 19:48:24 -0400

    fix whitespace

--- bsky-video-volume-slider.js
+++ bsky-video-volume-slider.js
@@ -6,51 +6,51 @@
 // ==/UserScript==
 
 const observer = new MutationObserver((mutations) => {
-  let hasNewVideos = false;
-  
+	let hasNewVideos = false;
+
 	for (let m of mutations) {
 		if (m.type === 'childList') {
 			for (let m2 of m.addedNodes) {
 				if (!m2 || !('querySelector' in m2)) continue;
 				let videoElements = m2.querySelectorAll('video');
-        hasNewVideos = (videoElements && videoElements.length) || hasNewVideos;
+				hasNewVideos = (videoElements && videoElements.length) || hasNewVideos;
 			}
 		}
 	}
-  
-  if (hasNewVideos) {
-    setTimeout(addVolumeSliders, 0);
-  }
+
+	if (hasNewVideos) {
+		setTimeout(addVolumeSliders, 0);
+	}
 });
 
 observer.observe(document.documentElement || document.body, {attributes: true, childList: true, subtree: true});
 
 function addVolumeSliders() {
-  const sliderClassName = '_bsky_volume_slider';
+	const sliderClassName = '_bsky_volume_slider';
 
-  let muteButtons = document.querySelectorAll("[aria-label='Unmute'],[aria-label='Mute']");
-  for (let node of muteButtons) {
-    if (node.nextSibling.classList.contains(sliderClassName)) {
-    	// slider already exists for this video
-      continue;
-    }
-    
-    // this is very silly
-    let vid = node.parentNode.parentNode.parentNode.parentNode.querySelector("video");
+	let muteButtons = document.querySelectorAll("[aria-label='Unmute'],[aria-label='Mute']");
+	for (let node of muteButtons) {
+		if (node.nextSibling.classList.contains(sliderClassName)) {
+			// slider already exists for this video
+			continue;
+		}
 
-    let slider = document.createElement("input");
-    slider.type = 'range';
-    slider.min = 0;
-    slider.max = 1;
-    slider.step = 'any';
-    slider.style.width = '5rem';
-    slider.classList.add(sliderClassName);
-    slider.addEventListener('input', (event) => {
-	    vid.volume = event.target.value;
-    });
+		// this is very silly
+		let vid = node.parentNode.parentNode.parentNode.parentNode.querySelector("video");
 
-    node.after(slider);
-  }
+		let slider = document.createElement("input");
+		slider.type = 'range';
+		slider.min = 0;
+		slider.max = 1;
+		slider.step = 'any';
+		slider.style.width = '5rem';
+		slider.classList.add(sliderClassName);
+		slider.addEventListener('input', (event) => {
+			vid.volume = event.target.value;
+		});
+
+		node.after(slider);
+	}
 }
 
 addVolumeSliders();
